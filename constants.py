9552e82fcd43018c2b731c408c7492842ee8abcf
Mrw33554432 <shaowen0219@outlook.com>
2023-04-16 21:48:41 +1000

    Prompt change to make GPT answer as required

--- constants.py
+++ constants.py
@@ -1,8 +1,4 @@
-command = """Please response following these rules:
-1. The selected request is bounded with !!!request!!!. Copy the selected request unchanged, start with <<start request>>, end with <<end request>>.
-2. You should analyze the intuition(why) of the response based on the given past conversation (consider the previous and following conversation), start with <<start analyze>>,end with <<end analyze>>.
-3. The selected response is bounded with ***response***. Write a better response with the given context. The new reply should be provided to replace the string bounded with ***response***. New response should start with <<new reply>> end with <<end reply>>.
-4. You should consider chats after the selected message as a feedback.
-Here is the conversation:"""
-
 api_key = ""
+
+
+
